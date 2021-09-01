2061b906feb9ca57f0b763948a19782cb57d1a61
Peter Nguyen <thong01677gmail.com@Peters-Air.lan>
2021-09-01 03:52:34 -0400

    complete the heading section

--- Training1/thongnq-training1/src/components/Header/Header.styles.js
+++ Training1/thongnq-training1/src/components/Header/Header.styles.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
-import img from "../../images/hero-img.jpg"
-
 export const Wrapper = styled.div`
+position: relative;
     min-height: 400px;
     max-width: 1760px;
     width: 100%;
+    height: 90vh;
 
     img{
         position: absolute;
         width: 100%;
+        height: 100%;
         left: 0px;
         right: 0px;
         object-fit: cover;
@@ -19,4 +20,71 @@ export const Wrapper = styled.div`
 `
 
 export const Content = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 1760px;
+    padding: 350px 80px 40px;
+
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+
+        div{
+            display: block;
+
+            .heading{
+                max-width: 500px;
+                text-align: center;
+
+                span{
+                    white-space: pre-line;
+                    max-width: 100%;
+                    font-size: 18px;
+                    font-weight: 600;
+                    letter-spacing: 0em;
+                    line-height: 22px;
+                    color: #222222;
+                }
+            }
+
+            .cta-button{
+                max-width: 500px;
+                display: flex;
+                justify-content: center;
+                margin-top: 16px;
+
+                a{
+                    display: inline-block;
+                    padding: 22px 56px;
+                    text-decoration:none;
+                    cursor: pointer;
+                    width: auto;
+                    background-color: #FFFFFF;
+                    font-size: 18px;
+                    line-height: 20px;
+                    font-weight: 600;
+                    border: initial;
+                    border-radius: 54px;
+                    outline: none;
+                    box-shadow: 0px 6px 16px rgb(0 0 0 / 12%);
+
+                    span{
+                        -webkit-text-fill-color: transparent;
+                        background: linear-gradient(90deg, #6F019C 0%, #C6017E 135.12%);
+                        -webkit-background-clip: text;
+
+                        font-weight: 800;
+                        color: #222222;
+                        line-height: 20px;
+                        max-height: 120px;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                    }
+                }
+            }
+        }
+    }
 `
--- Training1/thongnq-training1/src/components/Header/index.js
+++ Training1/thongnq-training1/src/components/Header/index.js
@@ -7,6 +7,20 @@ const Header = () => (
     <Wrapper>
         <Navbar />
         <img alt="" src="https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg?im_w=2560" />
+        <Content>
+            <div className="container">
+                <div>
+                    <div className="heading">
+                        <span>Not sure where to go?&nbsp;Perfect.</span>
+                    </div>
+                    <div className="cta-button">
+                        <a>
+                            <span>I'm flexible</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </Content>
     </Wrapper>
 );
 
