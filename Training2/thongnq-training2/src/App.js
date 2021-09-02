73f32815c6b9d6e70ce63873c58d16929b2691ff
Peter Nguyen <thong01677gmail.com@Peters-Air.lan>
2021-09-02 02:56:31 -0400

    completed the Task1

--- Training2/thongnq-training2/src/App.js
+++ Training2/thongnq-training2/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+
+import Modal from "./task1/Modal";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-500">Yooo</h1>
+      <Modal />
     </div>
   );
 }
---
+++ Training2/thongnq-training2/src/task1/Modal.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const Modal = () => {
+    const [state, setState] = useState();
+
+    return (
+        <div>
+            <h1>Not hello world!</h1>
+        </div>
+    )
+}
+
+export default Modal;
