1c88219e9703840159ea9aee20b2fdbd52707fc0
Peter Nguyen <thong01677gmail.com@Peters-Air.lan>
2021-09-14 07:43:07 -0400

    feat: create api service

---
+++ Training5/thongnq-training5-task2/src/services/user.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import authHeader from "./auth-header";
+
+const API_URL = "http://localhost:9000/api/users";
+
+class UserService {
+    getUserList(){
+        return axios.get(API_URL, {headers: authHeader()});
+    }
+
+    getUserInfo() {
+            return axios.get(API_URL + "/my", {headers: authHeader()});
+    }
+}
+
+export default new UserService();
