4ef2d12c3cc4cf277c7795311a9759580664da21
Peter Nguyen <thong01677gmail.com@Peters-Air.lan>
2021-09-15 15:37:55 -0400

    style: set up UI for the app

--- Training6/thongnq-training6-task2/src/App.js
+++ Training6/thongnq-training6-task2/src/App.js
@@ -1,7 +1,57 @@
+import React from "react";
+
+import {GlobalStyle, Wrapper, Content} from "./GlobalStyles";
+
 function App() {
   return (
     <div className="App">
+    
+      <Wrapper>
+        <Content>
+          <p className="network">Network: Online</p>
+          <p className="sign">Task Submission</p>
+          
+          <div className="form">
 
+            <input 
+              className="task" 
+              type="text" 
+              align="center" 
+              placeholder="Task Name"
+              />
+
+            <button className="submit" align="center">+ Task</button>
+
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Task Name</th>
+                <th>Status</th>
+              </tr>
+
+            </thead>
+
+            <tbody>
+
+              <tr>
+                <td>Backup DB</td>
+                <td>Ready</td>
+              </tr>
+              <tr>
+                <td>Backup DB</td>
+                <td>Ready</td>
+              </tr>
+              <tr>
+                <td>Backup DB</td>
+                <td>Ready</td>
+              </tr>
+            </tbody>
+          </table>
+        </Content>
+      </Wrapper>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ Training6/thongnq-training6-task2/src/GlobalStyles.js
@@ -0,0 +1,139 @@
+import {createGlobalStyle} from "styled-components";
+import styled from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+
+    *, *:before, *:after{
+        box-sizing: border-box;
+    }
+
+    html{
+        display: block;
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        line-break: strict;
+        font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+        font-size: 14px;
+        line-height: 1.43;
+        color: #484848;
+        background-color: #fff;
+        margin: 0;
+        -webkit-font-smooth: antialiased;
+    }
+
+
+`
+
+export const Wrapper = styled.div`
+    box-sizing: border-box;
+    width: 100vw;
+    height: 100vh;
+    background-color: #F3EBF6;
+    font-family: inherit;
+    padding: 70px;
+`
+
+export const Content = styled.div`
+    background-color: #FFFFFF;
+    width: 600px;
+    height: 600px;
+    border-radius: 1.5em;
+    box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+    margin: 0 auto;
+    position: relative;
+
+    .sign{
+        padding-top: 40px;
+        color: #8C55AA;
+        font-family: inherit;
+        font-weight: bold;
+        font-size: 23px;
+        text-align: center;
+    }
+
+    .network{
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        color: rgb(38, 50, 56);
+        font-weight: 700;
+        font-size: 14px;
+    }
+
+    .form{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    input{
+        width: 300px;
+        color: rgb(38, 50, 56);
+        font-weight: 700;
+        font-size: 14px;
+        letter-spacing: 1px;
+        padding: 10px 20px;
+        background: rgba(136, 126, 126, 0.04);
+        border: none;
+        border-radius: 20px;
+        outline: none;
+        box-sizing: border-box;
+        border: 2px solid rgba(0, 0, 0, 0.02);
+        text-align: center;
+        font-family: inherit;
+    }
+
+    input:focus {
+        border: 2px solid rgba(0, 0, 0, 0.18) !important;
+        
+    }
+
+    .submit{
+        cursor: pointer;
+        border-radius: 5em;
+        color: #fff;
+        background: linear-gradient(to right, #9C27B0, #E040FB);
+        border: 0;
+        padding: 10px 40px;
+        font-family: inherit;
+        font-size: 13px;
+        box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+        margin-left: 20px;
+    }
+
+    table {
+        margin: 30px auto;
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 90%;
+        border-radius: 20px;
+        padding: 20px;
+
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+        overflow: hidden;
+
+    }
+
+    thead tr{
+        background: linear-gradient(to right, #9C27B0, #E040FB);
+
+        color: #ffffff;
+        text-align: left;
+    }
+
+    td, th {
+        padding: 12px 15px;
+    }
+
+    tbody tr {
+        border-bottom: 1px solid #dddddd;
+    }
+
+    tr:nth-child(even) {
+        background-color: lightgray;
+    }
+
+`
